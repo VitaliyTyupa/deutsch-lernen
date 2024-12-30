0e2836f271e302f05e1954d9177067b88a303c70
Vit Tiupa <vitaliy.t@zazmic.com>
2024-12-30 22:14:59 +0100

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {
+  MatAnchor,
+  MatButton,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconButton,
+  MatMiniFabButton
+} from '@angular/material/button';
+import {MatDivider} from '@angular/material/divider';
+import {MatIcon} from '@angular/material/icon';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'dl-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbar,
+    MatIconButton,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'deutsch-lernen';
+
 }
