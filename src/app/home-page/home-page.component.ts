64169cf86eeb2f1be61355ca880f9321543bbcb1
Vit Tiupa <vitaliy.t@zazmic.com>
2025-07-01 19:58:33 +0200

    Added editors page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
-import {MatButton} from '@angular/material/button';
+import {Component, inject} from '@angular/core';
+import {MatButton, MatMiniFabButton} from '@angular/material/button';
 import {RouterLink} from '@angular/router';
+import {SessionService} from '../common-services/session.service';
+import {MatIcon} from '@angular/material/icon';
+import {NgIf} from '@angular/common';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardFooter,
+  MatCardHeader,
+  MatCardTitle
+} from '@angular/material/card';
 
 @Component({
   selector: 'dl-home-page',
   imports: [
     MatButton,
-    RouterLink
+    RouterLink,
+    MatIcon,
+    NgIf,
+    MatCardFooter,
+    MatCardContent,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    MatMiniFabButton,
+    MatCardActions
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+  private sessionService = inject(SessionService);
+  isLoggedIn$ = this.sessionService.isLoggedIn$;
 }
